47818f4b2c4cc491b0c9b55bb6227f4cc90c98ba
pawxyy <240109nb@gmail.com>
2025-06-29 22:13:41 +0300

    9 домашнее задание с канала SimpleCode

---
+++ main-9.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void Change(int *pa, int *pb){
+    int temp;
+    temp = (*pa);
+    (*pa) = (*pb);
+    (*pb) = temp;
+}
+
+int main(){
+    int a = 1, b = 2;
+    cout<< "a "<< a << endl;
+    cout<< "b "<< b << endl;
+
+    cout<<"Меняем местами.."<<endl;
+
+    Change(&a, &b);
+    cout<< "a "<< a << endl;
+    cout<< "b "<< b << endl;
+}
